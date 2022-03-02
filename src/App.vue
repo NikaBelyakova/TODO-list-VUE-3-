625f186944e9d5f55be06c98583fd10ffded805c
Nicalasha <vinch@yandex.ru>
2022-03-02 20:24:39 +0300

    code formatting

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <Header @add="addTask" />
-  <CardList @remove-item="removeTask" @item-done="toggleCompletion" :items="getUncompleteItems()" :itemsDone="getCompleteItems()" />
+  <CardList
+    @remove-item="removeTask"
+    @item-done="toggleCompletion"
+    :items="getUncompleteItems()"
+    :itemsDone="getCompleteItems()"
+  />
 </template>
 
 <script>
@@ -17,7 +22,7 @@ export default {
       items: [
         { title: "Задача первая", complete: false, id: Date.now() },
         { title: "Задача вторая", complete: false, id: Date.now() + 1 },
-      ]
+      ],
     };
   },
   methods: {
@@ -25,35 +30,35 @@ export default {
       this.items.push({
         title: valueInput,
         id: Date.now(),
-        complete: false
+        complete: false,
       });
     },
     removeTask(id) {
-    this.items = this.items.filter( item => item.id !== id);
+      this.items = this.items.filter((item) => item.id !== id);
     },
     getUncompleteItems() {
-    return this.items.filter(item => !item.complete)
+      return this.items.filter((item) => !item.complete);
     },
     getCompleteItems() {
-    return this.items.filter(item => item.complete)
+      return this.items.filter((item) => item.complete);
     },
-    toggleCompletion (item) {
-    item.complete = !item.complete;
-    }
+    toggleCompletion(item) {
+      item.complete = !item.complete;
+    },
   },
 };
 </script>
 
 <style >
-  * {
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-    margin: 0;
-    padding: 0;
-  }
+* {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  margin: 0;
+  padding: 0;
+}
 
- .container {
-    max-width: 800px;
-    margin: 0 auto;
-    padding: 0 15px;
-  }
+.container {
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 0 15px;
+}
 </style>
