d2a02c703319a8f63bf7f964c07c03045d56787f
Nicalasha <vinch@yandex.ru>
2022-03-02 14:38:06 +0300

    delete unnecessary comments

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,6 @@
 <template>
   <Header @add="addTask" />
-
   <CardList @remove-item="removeTask" @item-done="moveToDoneTask" :items="items" :itemsDone="itemsDone" />
-
-  <!-- <div class="container">
-        <h2>
-            <span>To Do</span>
-            <span class="mask-num">{{ items.length }}</span>
-        </h2>
-        <ul class="mask-list conplete-list">
-            <li 
-            v-for="(mask, index) in items" 
-            :key="mask.id"
-            >
-                <div>
-                    <input 
-                    type="checkbox" 
-                    @change="doCheck(index, 'need')">
-
-                    <span>{{ mask.title }}</span>
-                </div>
-                <button 
-                class="btn-remove"
-                @click="removeMask(index, 'need')"
-                >Remove</button>
-            </li>
-        </ul>
-
-        <h2>
-            <span>Done</span>
-            <span class="mask-num">{{ completeList.length }}</span>
-        </h2>
-        <ul class="mask-list conplete-list">
-            <li 
-            v-for="(mask, index) in completeList" 
-            :key="mask.id"
-            >
-                <div>
-                    <input type="checkbox" checked v-on:change="doCheck(index, 'complete')">
-                    <span>{{ mask.title }}</span>
-                </div>
-                <button class="btn-remove"
-                v-on:click="removeMask(index, 'complete')"
-                >Remove</button>
-            </li>
-        </ul>
-    </div> -->
 </template>
 
 <script>
@@ -90,16 +45,7 @@ export default {
           this.items.push(item);
           this.itemsDone = this.itemsDone.filter(item => item.complete === true);
         }
-    },
-    // doCheck(index, type) {
-    //   if (type === "need") {
-    //     const completeMask = this.items.splice(index, 1);
-    //     this.completeList.push(...completeMask);
-    //   } else {
-    //     const notCompleteMask = this.completeList.splice(index, 1);
-    //     this.items.push(...notCompleteMask);
-    //   }
-    // },
+    }
   },
 };
 </script>
